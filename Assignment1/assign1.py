b9f05731250d385566ebea1e9e1374a4539d4983
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-01-29 21:12:52 -0600

    intial assignments

---
+++ Assignment1/assign1.py
@@ -0,0 +1,11 @@
+#Study the chapter and answer the following question:
+#Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+#Be sure to enclose your operations in print() calls to see the results. You should create four lines that look like this:
+#print(5+3)
+#Your output should simply be four lines with the number 8 appearing once on each line.
+#you have to submit your .py file using this link.
+
+print(2+6) #addition
+print(11-3) #subtraction
+print(2*2*2) #multiplication
+print(64//8) #integer division

279830d8c001e6dbc7ebfa9958c1aa3b1b9c50b3
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-02-05 19:22:22 -0600

    week 2 hw

---
+++ Assignment2/assign2.py
@@ -0,0 +1,28 @@
+#Nigel Little
+#CITP 3305 v01
+#02/05/2022
+
+# Define 5 variables that store information such as: your first name, your last name, your A number, your age, and the class code.
+# Print the values for each of the following so that the output looks as follows:
+
+# My details:
+# Name: <first name> <last name>
+# A#: <your A number>
+# Age: <your age>
+# Class code: <your class code>
+
+# Note that there should be space between your first name and your last name. Store your middle name, if any, as part of your last name.
+# Each line in the output should be on a new line.
+# Compile and run the code using python 3
+
+first_name = 'Nigel'
+last_name = 'Little'
+a_num = 'A00644793'
+age = 28
+class_code = 'CITP 3305 v01'
+
+print("My details:")
+print("Name:", first_name, last_name)
+print("A#:", a_num)
+print("Age:", age)
+print("Class code:", class_code)

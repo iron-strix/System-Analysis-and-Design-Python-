c5715295250e0f39c972f5f71b7d025c933ba8c0
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-05-14 16:44:55 -0500

    added alpha checking and many comments

--- Semester Project/hangman.py
+++ Semester Project/hangman.py
@@ -1,8 +1,13 @@
 #Nigel Little
 #CITP 3305v01
 #03/05/2022
-#hangman test
+#hangman
 
+#this program will let the user play a game of hangman
+#features include: difficulty selection, web scraped hints from dictionary.com
+#replay selection, 85k words, and user scoring
+
+#imports
 import random
 import os
 import time
@@ -18,7 +23,8 @@ guessedList = []
 #create a global difficulty
 DIFFICULTY = 0
 
-#load the words from file
+#load the words from file, return list of words
+#list of words currently used is aprox 85k words
 def loadWordFile():
     inFile = open(WORDLIST_FILE, 'r')
     wordlist = []
@@ -34,7 +40,7 @@ def loadWordFile():
     #print("  ", len(wordlist), "words loaded.")
     return wordlist
 
-#have user choose difficulty, returns int
+#have user choose difficulty, returns int based on selection
 def chooseDifficulty():
     print(f"Please choose the level of difficulty desired.")
     print(f"1. Easy - Words are 6 characters or less. Hints are given.")
@@ -45,7 +51,7 @@ def chooseDifficulty():
     DIFFICULTY = int(difficulty)
     return int(difficulty)
 
-#choose a word at random based on difficulty from wordlist
+#choose a word at random based on difficulty from wordlist, returns a single string (secretword)
 def chooseWord(difficulty, wordlist):
     #print(f"difficulty chosen was {difficulty}")
 
@@ -84,6 +90,12 @@ def chooseWord(difficulty, wordlist):
 
 #web scraping
 #search
+#searches dictionary.com for the secretword and trims the url (if found) using regex
+#returns a string that *should* be just the definition of the word
+#due to different page layouts and imperfect regex (regex is hard, okay?), results may vary
+#so far pattern3 has proven the most reliable so it is the first regex attempted
+#if the regex pattern fails it returns a NoneType exception, hence the numerous try/except blocks
+#if the url 404, it likewise raises an exception
 def scrape(secretWord):  
     try:
         url = 'https://www.dictionary.com/browse/'
@@ -102,7 +114,11 @@ def scrape(secretWord):
         #result = re.sub("<.*?>", "", result) #remove HTML tags
         #result = result.replace("<span class=\"luna-example", "", 1)
         #return str(result)
-    
+
+        # ISSUES WITH ERRORS BEING THROWN
+        # HTML LAYOUT IS DIFFERENT FROM PAGE TO PAGE
+        # MAY NEED MULTIPLE REGEX TRY/CATCH BLOCKS
+        
         #pattern 3
         try:
             pattern = "value=\"1\".*?</div>"
@@ -153,9 +169,6 @@ def scrape(secretWord):
             print(e)
             #print(match_results)
 
-    # ISSUES WITH ERRORS BEING THROWN
-    # HTML LAYOUT IS DIFFERENT FROM PAGE TO PAGE
-    # MAY NEED MULTIPLE REGEX TRY/CATCH BLOCKS
     except Exception as e:
         print("Hint URL failed!")
         print (e)
@@ -168,6 +181,7 @@ def scrape(secretWord):
         playHangman(secretword)
 
 #check if guessed character is in word
+#if character is found returns true/false
 def checkSecretWord(secretWord, guess):
     if secretWord != None and guess in secretWord:
         print(f"{guess} found in the word!")
@@ -175,12 +189,24 @@ def checkSecretWord(secretWord, guess):
         return True
     else:
         print(f"{guess} is NOT in the word.")
-        time.sleep(1)
+        time.sleep(2)
         return False
 
-#check if character guessed is in list
+#checks if guess is in the alphabet, non-alpha characters aren't valid inputs
+#returns true if the guess is an alpha character, false if it is not
+def checkAlpha(guess):
+    alphaList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    for item in alphaList:
+        if item == guess.casefold():
+            return True
+
+    return False
+
+#check if guess has already been guessed
+#returns true if the guess is already in guessedList, false if not
 def checkGuessedList(guess):
     global guessedList
+    
     for item in guessedList:
         if item == guess:
             #print(f"{guess} has already been guessed!")
@@ -188,13 +214,19 @@ def checkGuessedList(guess):
     return False
     
 #keep track of which letters have been guessed
-#calls checkGuessedList to see first
+#calls checkGuessedList and checkAlpha to first check if it should be added to guessedList
+#since guessedList is used to create display, this is user facing and needs the extra check for things
+#to make sense for the user
 def addGuessedList(guess):
     global guessedList
-    if not checkGuessedList(guess):
+    if not checkGuessedList(guess) and checkAlpha(guess):
         guessedList.append(guess)
 
 #check guessedList vs secretWord
+#if all characters in the secretWord are also contained within guessedList then
+#the comprehension will return true/true/true/true etc for each character
+#essentially creating a new list of true/false values
+#if any of the characters fails to be found, the one false entry will cause the all() statement to return false
 def checkWinner(secretWord):
     global guessedList
     matchedList = [characters in guessedList for characters in secretWord]
@@ -203,11 +235,13 @@ def checkWinner(secretWord):
     return all(matchedList)
 
 #clear guesses when game is done
+#this list is user facing and used for display
 def clearGuessedList():
     global guessedList
     guessedList.clear()
 
 #display for user
+#main output to screen to play game of hangman
 def display(secretWord, lives):
     global guessedList
 
@@ -298,6 +332,8 @@ def display(secretWord, lives):
     print("\n")
 
 #calculate score
+#score dictionary is based on how common each of the characters is in typed words
+#based on a study of books that were transcribed from books to digital format
 def score(secretWord, lives):
     score = 0
     scoreDict = {
@@ -335,28 +371,62 @@ def score(secretWord, lives):
 
     return score*lives
 
+#return true/false based on user input
+#if true, player is prompted for another game
+#if false the program will exit
+def playAgain():
+    playAgain = ''
+    while playAgain != 'y' and playAgain != 'n':
+        playAgain = input("Play again? Y/N").casefold()
+        if playAgain != 'y' and playAgain != 'n':
+            print("Please input Y/N to make a selection.")
+            time.sleep(3)
+    
+    if playAgain == 'n':
+        print("Thank you for playing!")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        return False
+    else:
+        return True
+
 #main game logic 
 def playHangman(secretWord):
     global DIFFICULTY
-    lives = 7
+    lives = 7 #the number of guesses remaining, all difficulties have 7 lives
 
     #print(DIFFICULTY) #debug
     #input() #debug
 
+    #if the player has chosen easy, then difficulty == 1
+    #main change from other methods is that easy web scrapes hints for the player
     if DIFFICULTY == 1:
-        hint = scrape(secretWord)
+        hint = scrape(secretWord) #web scraping to store hint
+
+        #if there are lives remaining and the player has not won, continue game loop
         while lives > 0 and not checkWinner(secretWord):
-            display(secretWord, lives)
-            print(f"HINT: {hint}")
-            guess = input("Make a guess: ")[0]
-            addGuessedList(guess)
+            display(secretWord, lives) #print game screen
+            print(f"HINT: {hint}") #easy also prints the hint
+            guess = input("Make a guess: ")[0].casefold() #take input
+            addGuessedList(guess) #attempt to append input
 
-            if not checkSecretWord(secretWord, guess) and checkGuessedList(guess):
-                #print("Subtracting 1 life.")
-                lives -= 1
+            #first, check if the character is in the alphaList
+            #if the guess is not alpha, then no other checks need
+            #to be performed
+            if checkAlpha(guess): 
+                #check if guess is in the secret and check if it hasn't already been guessed
+                if not checkSecretWord(secretWord, guess) and checkGuessedList(guess):
+                    #print("Subtracting 1 life.")
+                    lives -= 1
+            else:
+                print("Guess is not in the alphabet. Please enter an alphabetical character (A-Z) as a guess.")
+                print("No guesses will be used.")
+                time.sleep(3)
 
+        #final update after either the player has won or the player has lost
         display(secretWord, lives)
 
+        #if the player has won, give them a score
         if checkWinner(secretWord):
             print(f"\nCongrats, you win! Here's your score: {score(secretWord, lives)}")
         else:
@@ -364,16 +434,26 @@ def playHangman(secretWord):
         
         clearGuessedList()
 
+    #if the difficulty is not easy, then all the above logic follows except hints will not be given
     else:
         while lives > 0 and not checkWinner(secretWord):
             display(secretWord, lives)
 
-            guess = input("Make a guess: ")[0]
+            guess = input("Make a guess: ")[0].casefold()
             addGuessedList(guess)
 
-            if not checkSecretWord(secretWord, guess) and checkGuessedList(guess):
-                #print("Subtracting 1 life.")
-                lives -= 1
+            #first, check if the character is in the alphaList
+            #if the guess is not alpha, then no other checks need
+            #to be performed
+            if checkAlpha(guess): 
+                #check if guess is in the secret and check if it hasn't already been guessed
+                if not checkSecretWord(secretWord, guess) and checkGuessedList(guess):
+                    #print("Subtracting 1 life.")
+                    lives -= 1
+            else:
+                print("Guess is not in the alphabet. Please enter an alphabetical character (A-Z) as a guess.")
+                print("No guesses will be used.")
+                time.sleep(3)
 
         display(secretWord, lives)
 
@@ -384,19 +464,17 @@ def playHangman(secretWord):
         
         clearGuessedList()
 
+#main calls the main game logic and also handles the game state (playing/not playing)
 def main():
     playing = True
     while playing:
-        secretWord = chooseWord(chooseDifficulty(), loadWordFile())
+        secretWord = chooseWord(chooseDifficulty(), loadWordFile()) #get a random word out of the lists and prompt the player to choose a difficulty
         #secretWord = input("DEBUG: Input secret word. Please disable this feature to actually play a game.")
         #print(f"I'm cheating. secretWord is {secretWord}")
-        playHangman(secretWord)
+        #time.sleep(3)
+        playHangman(secretWord) #using the random secretword, play a game of hangman with the chosen difficulty. Difficulty is global and is not passed
         
-        playAgain = ''
-        while playAgain != 'y' and playAgain != 'n':
-            playAgain = input("Play again? Y/N").casefold()
-        
-        if playAgain == 'n':
-            playing = False
-    
+        playing = playAgain() #prompt to play again
+        os.system('cls' if os.name == 'nt' else 'clear') #finally, clean up screen
+  
 main()

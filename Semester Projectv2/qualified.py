009297051bd3ec7da1dc20d8d470c375649d694b
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-05-14 20:14:02 -0500

    added comments

--- Semester Projectv2/qualified.py
+++ Semester Projectv2/qualified.py
@@ -1,3 +1,6 @@
+#list of qualified names used in other portions of the program
+#essentially global variables
+
 #define where the words file is
 global WORDLIST_FILE 
 WORDLIST_FILE = "engmix_removed.txt"
